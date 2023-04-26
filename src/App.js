3950bdaf18e327131b2713096c422e3e02fe9093
Ethan <ethan.nordahl1@gmail.com>
2023-04-26 01:00:51 -0700

    firebase best practices

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { auth } from "./firebase/init";
+import { auth, db } from "./firebase/init";
+import { collection, addDoc, getDocs, getDoc, doc, query, where, updateDoc, deleteDoc } from "firebase/firestore"
 import companyLogo from "./Assets/Frontend Simplified Logo.853fbda.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -14,6 +15,53 @@ import {
 function App() {
   const [user, setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
+
+  async function updatePost () {
+    const hardcodedId = "CXQtsBxWb2cPfWYcp1WN"
+    const postRef = doc(db, "posts", hardcodedId);
+    const post = await getPostById(hardcodedId);
+    const newPost = {
+      ...post,
+      title: "Land a $400k job"
+    };
+    updateDoc(postRef, newPost);
+  }
+  
+  function deletePost() {
+    const hardcodedId = "CXQtsBxWb2cPfWYcp1WN"
+    const postRef = doc(db, "posts", hardcodedId);
+    deleteDoc(postRef);
+  }
+
+  function createPost() {
+    const post = {
+      title: "Finish Interview Section",
+      description: "Do Frontend Simplified",
+      uid: user.uid,
+    };
+    addDoc(collection(db, "posts"), post)
+  }
+
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, "posts"));
+    const posts = docs.map(elem => ({...elem.data(), id: elem.id}));
+    console.log(posts)
+  }
+
+  async function getPostById(id) {
+    const postRef = doc(db, "posts", id)
+    const postSnap = await getDoc(postRef);
+    return postSnap.data();
+  }
+
+  async function getPostByUid() {
+    const postCollectionRef = await query(
+      collection(db, "posts"),
+      where("uid" , "==", user.uid)
+    );
+    const { docs } = await getDocs(postCollectionRef);
+    console.log(docs);
+  }
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -85,6 +133,12 @@ function App() {
           </>
         )}
       </div>
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
+      <button onClick={getPostById}>Get Post By Id</button>
+      <button onClick={getPostByUid}>Get Post By Uid</button>
+      <button onClick={updatePost}>Update Post</button>
+      <button onClick={deletePost}>Delete Post</button>
     </nav>
   );
 }
